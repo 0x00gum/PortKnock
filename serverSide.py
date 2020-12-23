e8a654436b520841b8d3d49fca68875e69cc7d51
TheSerialiZator <69147968+TheSerialiZator@users.noreply.github.com>
2020-12-23 15:20:21 +0200

    Update serverSide.py

--- serverSide.py
+++ serverSide.py
@@ -18,15 +18,17 @@ class PortKnocking(object):
         parser.add_argument('-l', '-listen', type=int,
                             help='Port to listen for incoming knocks.',
                             required=True)
-        parser.add_argument('-i', '--clientIp', help='client Ip.', required=True)
+        parser.add_argument('-i', '--clientIp',
+                            help='client Ip.', required=True)
         parser.add_argument('-n', '--numberOfknocks',
-                            help='number of knowks to except form the client(default is 4): -n 4',
+                            help='number of knocks to except form the client(default is 4): -n 4',
                             default=4,
                             required=False)
         parser.add_argument('-f', '--flag',
                             help='set a type of a flag to except form the client: -f ACK or -f SYN or -f RST.',
                             required=True)
-        parser.add_argument('-c', '--cipher', help='cipher Key to decrypt the password (avoid MITM).', required=False)
+        parser.add_argument(
+            '-c', '--cipher', help='cipher Key to decrypt the password (avoid MITM).', required=False)
         parser.add_argument('-p', '--password',
                             help='Set a password to authenticate with the client.',
                             required=True)
@@ -42,44 +44,43 @@ class PortKnocking(object):
         self.password = args.password
         self.timeout = args.timeout
 
-    def get_encrypted_string(self):
-        
-        # Server is listening for encrypted password and returns the value.
-        print("[+] Server is listening for incoming knowcks.")
-        data = sniff(count=1, filter=f"icmp and host {self.clientIp}")
+    def get_encrypted_string(self) -> str:
+        """Server is listening for encrypted password and returns the value."""
+        print("[+] Server is listening for incoming knocks.")
+        data = sniff(count=1, filter=f"ICMP and host {self.clientIp}")
         firstIndex = str(data[0])
         for i in firstIndex:
             li = list(firstIndex.split(" "))
-        string_to_decrypt = str(li[4])
+        string_to_decrypt: str = li[4]
+
         return string_to_decrypt
 
     def decrypt_icmp_packet(self):
-        # Decrpyting the icmp packets sent by the client by using Fernet.
+        """Decrypting the ICMP packets sent by the client by using Fernet."""
         string_to_decrypt = self.get_encrypted_string()
         cipher = Fernet(self.cipher)
         encoded = string_to_decrypt.encode()
         decrypted_text = cipher.decrypt(encoded)
+
         return decrypted_text
 
-    def decrypt_password(self):
-
-        # Obviously decrypting the password! 
+    def decrypt_password(self) -> bool:
+        """Obviously decrypting the password!"""
         decrypted_text = self.decrypt_icmp_packet()
         bytes_pass = bytes(self.password, encoding='utf-8')
         if decrypted_text == bytes_pass:
             return True
-        elif decrypted_text != bytes_pass:
-            return False
+        
+        return False
 
-    def sniff_packets(self):
-        """
-            This function sniffs the packets form the client if
-            the password is correct.
-        """
+    def sniff_packets(self) -> None:
+        """This function sniffs the packets form the client if
+        the password is correct."""
         if self.decrypt_password():
             print("[+] Password is correct!")
             sniff_filter = f"tcp and host {self.clientIp} and port {self.port}"
-            sniffed_packets = sniff(count=self.numberOfknocks * 2, filter=sniff_filter)
+            sniffed_packets = sniff(
+                count=self.numberOfknocks * 2, filter=sniff_filter)
             if sniffed_packets != " ":
                 print("[+] Opening SSH port for:", self.timeout, "minutes.")
                 try:
@@ -90,6 +91,7 @@ class PortKnocking(object):
                 except KeyboardInterrupt:
                     subprocess.call(['service', 'ssh', 'stop'])
                     sys.exit()
+
 
 # Starting the program with arguments!
 PortKnocking(sys.argv[1:]).sniff_packets()
